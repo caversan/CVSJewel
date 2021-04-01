506b15a3a305c1a238658a8bd81b79b1d95d55eb
Adriano Caversan <adriano.caversan@gmail.com>
2021-03-31 23:52:37 -0300

    - Moving Files - Prefab loading function - cell prefabLoader - camera adjustment - header in main script and readme.md file

--- Assets/Scripts/MainScript.cs
+++ Assets/Scripts/MainScript.cs
@@ -1,3 +1,23 @@
+/*
+CVSJewel ver:0.01b
+
+Author: Adriano Caversan
+E-mail: adriano.caversan@gmail.com
+Date: Mar/30/2021
+
+Unity3D ver: 2020.3.1f1 Personal
+
+Description: Simple match 3 game made with Unity3D for study and portifolio purposes, the commercial use is not allowed.
+
+3rd party assets:
+-Gems: "2D FREE Crystal Set" by diluck from Unity Asset Store "thank you for this great assets" (https://assetstore.unity.com/packages/2d/textures-materials/2d-free-crystal-set-175156)
+
+*/
+
+
+
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,16 +26,39 @@ using UnityEngine;
 public class MainScript : MonoBehaviour
 {
     
-    /*public GameObject hitNeighbor (GameObject obj)
-    {   
-        return obj;
-    }*/
 
     //column size, row size, col length, row lenght, crystal identifier;
     public int colW,rowH,colLen,rowLen,crystalId;
-    public GameObject [,] CrystalsList;
+
+    //List for loading the possible crystals prefabs
     public List<GameObject> Crystals = new List<GameObject>();
+
+    //Cell prefab loader
+    public List<GameObject> Cells = new List<GameObject>();
+
+    //List for populate the canvas with the crystals grid
+    public GameObject [,] CrystalsList;
     
+    public void prefabLoader (string folder, List<GameObject> thisList)
+    {   
+        //Prefab loader, load all prefabs from the target folder and populate the target
+        GameObject objToLoad = null;
+        int counter = 0;
+        bool done = false;
+
+        while(!done)
+        {
+                objToLoad = Resources.Load(folder + counter) as GameObject;
+                if(objToLoad == null)
+                {
+                    done = true;
+                } else{
+                    thisList.Add(objToLoad);
+                }
+                ++counter;
+        }
+    }
+
 
 
     void Start()
@@ -28,27 +71,15 @@ public class MainScript : MonoBehaviour
         colLen=8;
         //number of rows
         rowLen=8;
+        //Determining the grid size
         CrystalsList = new GameObject[colLen,rowLen];
-
-
-        //Resource loader, load all crystals prefabs from the Prefab/Crystals folder
-        GameObject crystal = null;
-        int counter = 0;
-        bool done = false;
-
-        while(!done)
-        {
-                crystal = Resources.Load("Crystals" + counter) as GameObject;
-                if(crystal == null)
-                {
-                    done = true;
-                } else{
-                    Crystals.Add(crystal);
-                }
-                ++counter;
-        }
-
-
+        
+        //Prefab loader, load all crystals prefabs from the Prefab/Crystals folder
+        prefabLoader ("Prefabs/Crystals", Crystals);
+        //Prefab loader, load all cells prefabs from the Prefab/Crystals folder
+        prefabLoader ("Prefabs/UI/Cells" , Cells);
+      
+        
         //This 'for' bellow instantiate the crystals in the canvas preventing a repetitive crystals sequence in the first turn.
         
         //There we increment lines
@@ -79,14 +110,8 @@ public class MainScript : MonoBehaviour
                         //No chance to sort a same crystal that was destroyed before because I walk in the list step by step from the end to the beginning. Certally, in this case, the destroyed crystal had another index.
                         crystalId = (crystalId==0 ? crystalId=Crystals.Count-1 : crystalId-1);                        
                         CrystalsList[i,j]=(GameObject)Instantiate(Crystals[crystalId], new Vector2(colW*j,rowH*i), Quaternion.identity);
-                        Debug.Log("passei por aqui");
                     }
                 }
-
-
-
-                //Debug.Log(hitNeighbor(crystal).ToString());
-                //Debug.Log(CrystalsList[i,j].name);
             }
         }
     }
